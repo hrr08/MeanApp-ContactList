cc85b22ce2d18afc4a543dd993dc26aed3d557d7
hrr08 <harish.r.ramalingam@gmail.com>
2017-03-05 15:41:24 +0800

    create angular web app. Updated all API at server side

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var CONTACTS_COLLECTION = 'contacts';
 var app = express();
 app.use(bodyParser.json());
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 var db;
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
@@ -51,6 +55,7 @@ function handleError(res, reason, message, code) {
  
  app.post('/api/contacts', function(req, res) {
 	var newContact = req.body;
+	newContact.createDate = new Date();
 	
 	if(!req.body.name) {
 		handleError(res, 'Invalid user input', 'Must provide a name.', 400);
@@ -73,11 +78,39 @@ function handleError(res, reason, message, code) {
   */
  
  app.get('/api/contacts/:id', function(req, res) {
+	db.collection(CONTACTS_COLLECTION).findOne({_id = new ObjectID(req.params.id)}, function(err, doc) {
+		if(err)  {
+			handleError(res, err.message, "Failed to get contact");
+		}
+		else {
+			res.status(200).json(doc);
+		}
+	});
  });
  
  app.put('/api/contacts/:id', function(req, res) {
+	var updateDoc = req.body;
+	delete	updateDoc._id;
+	
+	db.collection(CONTACTS_COLLECTION).updateOne({_id = new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+		if(err) {
+			handleError(res, err.message, "Failed to update contact");
+		}
+		else {
+			updateDoc._id = req.params.id;
+			res.status(200).json(updateDoc);
+		}
+	});
  });
  
+ 
  app.delete('/api/contacts/:id', function(req, res) {
+	db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+		if (err) {
+		  handleError(res, err.message, "Failed to delete contact");
+		} else {
+		  res.status(200).json(req.params.id);
+		}
+	}); 
  });
  
